14d699951cc79c030c57b36309230c3914614e71
0l1ve <ocollioud@gmail.com>
2017-01-29 18:16:54 +0100

    Script to extract superhero and star wars data in fr and en

--- extract_superhero_starwars.py
+++ extract_superhero_starwars.py
@@ -1,34 +1,55 @@
-import json
-from django.db.models import Q
-#from django.core import serializers
-from apps.gcd.models import Story, Issue
+import os
 
-stories = Story.objects.filter(
-    Q(issue__series__language__code__in=["fr", "en"]),
-    Q(title__icontains="Star Wars") | Q(genre__icontains="superhero")
-).exclude(issue__series__publication_type__name="magazine").values("issue").distinct()
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    import json
+    import django
+    from django.db.models import Q
+    #from django.core import serializers
+    from apps.gcd.models import Story, Issue
 
-# Count as of 19.01.2017: 32327
+    django.setup()
+    stories = Story.objects.filter(
+        Q(issue__series__language__code__in=["fr", "en"]), Q(issue__variant_of=None),
+        Q(type__name="cover"),
+        Q(title__icontains="Star Wars") | Q(genre__icontains="superhero")
+    ).exclude(issue__series__publication_type__name="magazine")
 
-# data = serializers.serialize('json', [Issue.objects.get(id=story['issue']) for story in stories])
+    # print stories.values("issue").distinct().count()
+    # Count as of 19.01.2017: 25219
 
-data = []
-for story in stories:
-    id=story['issue']
-    issue = Issue.objects.get(id=id)
-    data.append{
-        'id': id,
-        'lang': issue.series.language.code,
-        'genre': issue.story_set.first().genre,
-        'short_name': issue.short_name(),
-        'full_name': issue.full_name(),
-        'gcd_url': issue.get_absolute_url(),
-        'page_count': page_count,
-        'price': price,
-        'publication_date': publication_date,
-        'barcode': barcaode,
-        'valid_isbn': valid_isbn,
-        'isbn': isbn
-    }) 
+    # data = serializers.serialize('json', [Issue.objects.get(id=story['issue']) for story in stories])
 
-json.dumps(data, indent=4)
+    ids = set()
+    with open('data.json', 'w') as f:
+        f.write("[")
+        for story in stories:
+            issue = story.issue
+            id=issue.id
+            if id in ids:
+                continue
+            ids.add(id)
+            data = {
+                'lang': issue.series.language.code,
+                'country': issue.series.country.code,
+                'publisher': issue.series.publisher.name,
+
+                'genre': story.genre,
+                'script': story.script,
+                'pencils': story.pencils,
+                'characters': story.characters,
+                'synopsis': story.synopsis,
+
+                'id': str(id),
+                'short_name': issue.short_name(),
+                'full_name': issue.full_name(),
+                'gcd_url': issue.get_absolute_url(),
+                'page_count': str(issue.page_count),
+                'price': issue.price,
+                'publication_date': issue.publication_date,
+                'barcode': issue.barcode,
+                'valid_isbn': issue.valid_isbn
+            }
+
+            f.write(json.dumps(data, indent=4))
+        f.write("]")
